35068728891103fd68cd1aff94691b2817b64948
aron <mosa@hassemprag.com>
2024-09-08 22:21:42 +0200

    Added two models that are essential for the project to align with project structure

---
+++ CMCS/Models/Lecturer.cs
@@ -0,0 +1,11 @@
+﻿namespace CMCS.Models
+{
+    public class Lecturer
+    {
+        public int LecturerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public decimal HourlyRate { get; set; }
+    }
+}
