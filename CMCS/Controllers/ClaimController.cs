976dbb16b9549227663da2bd8e5ae01e720d2b35
aron <mosa@hassemprag.com>
2024-09-08 22:17:11 +0200

    Added controller to handle the requests and named it claim controller

---
+++ CMCS/Controllers/ClaimController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMCS.Models;
+namespace CMCS.Controllers
+{
+    public class ClaimController : Controller
+    {
+        // Lecturer submits a claim
+        public IActionResult SubmitClaim()
+        {
+            return View();
+        }
+
+        // Coordinator approves or rejects a claim
+        public IActionResult ApproveClaim()
+        {
+            return View();
+        }
+
+        // Lecturer tracks the status of their claim
+        public IActionResult TrackClaim()
+        {
+            return View();
+        }
+    }
+}
